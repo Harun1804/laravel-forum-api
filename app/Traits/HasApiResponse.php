f18e489424b787fe3da2637a33dd91b513664213
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2024-03-01 13:34:15 +0700

    Add response formatter

---
+++ app/Traits/HasApiResponse.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+trait HasApiResponse
+{
+    protected static $response = [
+        'meta' => [
+            'code'      => 200,
+            'status'    => true,
+            'message'   => null,
+        ],
+        'result' => null
+    ];
+
+    public static function successReponse($data = null, $message = null, $code = 200)
+    {
+        self::$response['meta']['code']      = $code;
+        self::$response['meta']['message']   = $message;
+        self::$response['data']              = $data;
+
+        return response()->json(self::$response, self::$response['meta']['code']);
+    }
+
+    public static function errorResponse($data = null, $message = null, $code = 500)
+    {
+        self::$response['meta']['code']      = $code;
+        self::$response['meta']['status']    = false;
+        self::$response['meta']['message']   = $message;
+        self::$response['data']              = $data;
+
+        return response()->json(self::$response, self::$response['meta']['code']);
+    }
+
+    public function validationResponse($errors = null, $message = 'Validation Errors', $code = 422)
+    {
+        self::$response['meta']['code']     = $code;
+        self::$response['meta']['status']   = false;
+        self::$response['meta']['message']  = $message;
+        self::$response['data']             = $errors;
+
+        throw new HttpResponseException(response()->json(self::$response, self::$response['meta']['code']));
+    }
+}
