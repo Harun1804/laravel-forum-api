2824303408d0ea17f4693e68f3c5bbbb47e6f99b
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2024-03-05 07:10:34 +0700

    change feed order

--- app/Http/Controllers/Api/Feed/FeedController.php
+++ app/Http/Controllers/Api/Feed/FeedController.php
@@ -15,7 +15,7 @@ class FeedController extends ApiController
     public function index(Request $request)
     {
         try {
-            $feeds = Feed::with('user')->get();
+            $feeds = Feed::with('user')->latest()->get();
             return $this->successResponse($feeds, 'Feeds retrieved successfully');
         } catch (\Exception $e) {
             return $this->errorResponse($e->getMessage());
